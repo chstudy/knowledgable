08f9b0e627218cfda2532e32e7fcb1c300893503
chenglongzhang <zhangchenglong2@sfmail.sf-express.com>
2022-11-16 17:27:32 +0800

    update

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,14 +1,14 @@
 module.exports = [
     {
-        text: '自习路线', link: '/guide/',
+        text: 'Java知识结构体系', link: '/guide/',
         items: [
             {text: 'Java基础篇', link: '/interview/java-se/'},
-            {text: 'JavaWeb篇', link: '/interview/java-web/'},
+            {text: 'JavaWeb篇', link: '/interview/java-web/'}
         ]
     },
     {
-        text: '面试宝典',
-		items: [
+        text: 'Java面试秘籍',
+        items: [
             {text: 'Java基础篇', link: '/interview/java-se/'},
             {text: 'JavaWeb篇', link: '/interview/java-web/'},
         ]
@@ -16,25 +16,27 @@ module.exports = [
     {
         text: '工具箱',
         items: [
-			{
+            {
                 text: '在线编辑',
-				items: [
-					{text: '图片压缩', link: 'https://tinypng.com/'}
-				]
+                items: [
+                    {text: '图片压缩', link: 'https://tinypng.com/'}
+                ]
             },
-			{
-                text: '在线服务',
-				items: [
-					{text: '阿里云', link: 'https://www.aliyun.com/'},
-					{text: '腾讯云', link: 'https://cloud.tencent.com/'}
-				]
+            {
+                text: '常用在线工具',
+                items: [
+                    {text: '时间戳转换', link: 'https://tool.lu/timestamp/'},
+                    {text: 'JSON格式化/Unicode转换', link: 'https://www.sojson.com/'}
+                ]
             },
-			{
+            {
                 text: '博客指南',
-				items: [
-					{text: '掘金', link: 'https://juejin.im/'},
-					{text: 'CSDN', link: 'https://blog.csdn.net/'}
-				]
+                items: [
+                    {text: '掘金', link: 'https://juejin.im/'},
+                    {text: 'CSDN', link: 'https://blog.csdn.net/'},
+                    {text: '博客园', link: 'https://www.cnblogs.com/'},
+                    {text: '简书', link: 'https://www.jianshu.com/'},
+                ]
             }
         ]
     }
